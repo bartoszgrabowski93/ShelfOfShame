1a5080e19ec394e76b3da482a0d584d4ec374804
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-11-05 15:17:52 +0100

    Adding models into domain layer

---
+++ ShelfOfShame.Domain/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelfOfShame.Domain.Models
+{
+    public abstract record Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserRating { get; set; }
+        public int CriticsRating { get; set; }
+        
+    }
+}
