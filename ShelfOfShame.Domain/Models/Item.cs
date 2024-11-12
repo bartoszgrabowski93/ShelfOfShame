6063cfa2f7085aa5d542c257e5e13585b9bec79f
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-11-12 17:09:00 +0100

    Changes in app and models

--- ShelfOfShame.Domain/Models/Item.cs
+++ ShelfOfShame.Domain/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShelfOfShame.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ShelfOfShame.Domain.Models
 {
-    public abstract class Item
+    public abstract class Item : IItem
     {
         public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
